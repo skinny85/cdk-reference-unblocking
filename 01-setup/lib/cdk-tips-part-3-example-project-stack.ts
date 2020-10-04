300e427e2d09a14c77ff3598fa212f1048b0aaa7
Adam Ruka <adamruka85@gmail.com>
2020-10-03 17:36:42 -0700

    Move the code to 01-setup.

--- 01-setup/lib/cdk-tips-part-3-example-project-stack.ts
+++ 01-setup/lib/cdk-tips-part-3-example-project-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class CdkTipsPart3ExampleProjectStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new s3.Bucket(this, 'Bucket');
   }
 }
