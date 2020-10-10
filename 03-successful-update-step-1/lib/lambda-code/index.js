eba469d245a5fe4e7ffbdb969ce23705225bee6a
Adam Ruka <adamruka85@gmail.com>
2020-10-10 15:48:33 -0700

    Add a comment to the code of the Lambda reading from a DynamoDB Table.

--- 03-successful-update-step-1/lib/lambda-code/index.js
+++ 03-successful-update-step-1/lib/lambda-code/index.js
@@ -4,6 +4,9 @@ exports.handler = async function (event) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     const dynamoDb = new Aws.DynamoDB();
 
+    // in this example, we'll just do a simple DescribeTable call;
+    // of course, in a  real application,
+    // you would use the Table to store and retrieve data
     const tableDetails = await dynamoDb.describeTable({
         TableName: process.env.TABLE_NAME,
     }).promise();
